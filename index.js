688f82bd98cedd3b666fb5b1ea109368392fffa4
Gold Usenekong <thatdevg@Golds-MBP.attlocal.net>
2022-01-06 21:58:52 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Lesson 1: COMMENT YOUR JAVASCRIPT CODE
+
+// Lesson 1.1 SINGLE LINE CODE
+
+// To comment out a Single Line code use the '//' aka 'Forward Slash' before the code.
+
+// Another way to comment out a Single Line code is to press Command + Forward Slash (Command + /)
+
+// Example: SINGLE LINE CODE
+
+console.log ("I am testing the comment code function using this line") 
+
+// console.log ("I am testing the comment code function using this line")
+
+// Lesson 1.2 MULTI LINE CODE
+
+// To comment out a Multi Line code use the '/*' in the first line and end with "*/" in the second line after the code.
+
+// Example: MULTI LINE CODE
+
+if (1 === 1) {
+    "I am testing the comment code function using this line"
+}
+
+/* if (1 === 1) {
+    "I am testing the comment code function using this line"
+} */
+
+/* console.log ("As a line,
+I am commented out") */
+
+
+
